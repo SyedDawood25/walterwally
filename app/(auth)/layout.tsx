069c2012cf15ed6bd071e54a84fb41629cc00cd7
Moiz Habib <moizhabib9602@gmail.com>
2024-05-25 09:43:28 +0500

    fixed authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,29 +9,33 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <main className="flex h-screen bg-[#0A100D]">
-      <div className="flex flex-col w-1/2">
-        <div className="flex absolute">
+    <main className="flex justify-center items-center h-screen bg-[#090F0C]  sm:bg-[#090F0C]">
+      <div className="flex flex-col justify-center items-center sm:w-1/2">
+        <div className="bg-[#090F0C] hidden sm:flex sm:absolute">
           <Image
             src={Background}
             alt="Background"
-            className="h-screen object-cover"
+            className=" sm:h-screen sm:object-cover"
           />
+          <div className=""></div>
         </div>
 
-        <div className="flex flex-col justify-evenly my-16 gap-y-8 h-full relative p-20">
-          <div className="w-[400px]">
+        <div className="hidden sm:flex flex-col justify-evenly my-2 sm:my-16  gap-y-8 sm:h-full sm:relative p-20">
+          <div className="w-[150px] sm:w-[300px]">
             <Image src={Logo} alt="Logo" />
           </div>
 
-          <div className="flex flex-col text-white">
-            <h3 className="text-6xl font-semibold">Unleash your</h3>
-            <h2 className="text-8xl font-semibold">Imagination.</h2>
+          <div className="hidden sm:flex sm:flex-col text-white">
+            <h3 className="text-6xl md:text-4xl font-semibold">Unleash your</h3>
+            <h2 className="text-8xl md:text-6xl font-semibold">Imagination.</h2>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-center items-center w-1/2 bg-[#090F0C]">
+      <div className="flex flex-col justify-center items-center sm:w-1/2 bg-[#090F0C] gap-y-[80px]">
+        <div className="sm:hidden w-[150px] sm:w-[300px]">
+          <Image src={Logo} alt="Logo" />
+        </div>
         {children}
       </div>
     </main>
--- app/(auth)/login/[[...login]]/page.tsx
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -1,8 +1,10 @@
 import { FormWrapper } from "@/components/FormWrapper";
 import { LoginForm } from "@/components/loginform/LoginForm";
+import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
-  return <LoginForm />;
+  return <SignIn />;
+  // return <div></div>
 };
 
 export default SignInPage;
